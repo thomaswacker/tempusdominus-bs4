39c18d4eecc473b83a535c7b1a49dc6da3667421
thomas.wacker <thomas.wacker@etailer.de>
2019-05-22 11:56:01 +0200

    Fixed: Event "show.datetimepicker" not fired

--- src/docs/theme/js/tempusdominus-bootstrap-4.js
+++ src/docs/theme/js/tempusdominus-bootstrap-4.js
@@ -1,6 +1,6 @@
 /*@preserve
  * Tempus Dominus Bootstrap4 v5.1.2 (https://tempusdominus.github.io/bootstrap-4/)
- * Copyright 2016-2018 Jonathan Peterson
+ * Copyright 2016-2019 Jonathan Peterson
  * Licensed under MIT (https://github.com/tempusdominus/bootstrap-3/blob/master/LICENSE)
  */
 
